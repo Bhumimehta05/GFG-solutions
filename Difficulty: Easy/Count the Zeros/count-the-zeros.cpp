890c9dd0f22dee528163a396da406ec300578a18
Bhumimehta05 <mehtabhumi025@gmail.com>
2026-07-12 10:49:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count the Zeros/count-the-zeros.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int countZeroes(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0)
+            count+=1;
+        }
+        return count;
+    }
+};
